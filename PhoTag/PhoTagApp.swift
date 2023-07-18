62b3c7b9c0d285967ed6bd6d6d345857fae8557d
alfraser <alisdairfraser@gmail.com>
2023-07-18 12:49:21 +0100

    Initial Commit

---
+++ PhoTag/PhoTagApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhoTagApp.swift
+//  PhoTag
+//
+//  Created by Al Fraser on 18/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct PhoTagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
