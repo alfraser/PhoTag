948449bb452e83faa531de732075924fa988dfd4
alfraser <alisdairfraser@gmail.com>
2023-07-22 16:01:45 +0100

    Added optional location to the model code and flowed it through the view hierarchy, plus added some additional previews

--- PhoTag/Model/Model.swift
+++ PhoTag/Model/Model.swift
@@ -6,11 +6,14 @@
 //
 
 import Foundation
+import MapKit
 import UIKit
 
 struct TaggedPhoto: Identifiable, Codable, Equatable, Comparable {
     var id = UUID()
     var description = ""
+    var latitude: Double?
+    var longitude: Double?
     
     var image: UIImage? {
         if let img = UIImage(named: id.uuidString) {
@@ -20,6 +23,12 @@ struct TaggedPhoto: Identifiable, Codable, Equatable, Comparable {
             return img
         }
         return nil
+    }
+    
+    var location: CLLocationCoordinate2D? {
+        guard let latitude = latitude else { return nil }
+        guard let longitude = longitude else { return nil }
+        return CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
     }
     
     static let example = TaggedPhoto(id: UUID(), description: "Example Photo")
--- PhoTag/View/TaggedPhotoDetailView.swift
+++ PhoTag/View/TaggedPhotoDetailView.swift
@@ -5,6 +5,7 @@
 //  Created by Al Fraser on 19/07/2023.
 //
 
+import MapKit
 import SwiftUI
 
 struct TaggedPhotoDetailView: View {
@@ -14,7 +15,7 @@ struct TaggedPhotoDetailView: View {
     var body: some View {
         ZStack {
             if let img = taggedPhoto.image {
-                PhotoDetailView(photo: img, description: taggedPhoto.description)
+                PhotoDetailView(photo: img, description: taggedPhoto.description, location: taggedPhoto.location)
             } else {
                 MissingPhotoView(description: taggedPhoto.description)
             }
@@ -26,11 +27,13 @@ struct TaggedPhotoDetailView: View {
 }
 
 struct TaggedPhotoDetailView_Previews: PreviewProvider {
-    static var exampleWithPhoto = TaggedPhoto(id: UUID(uuidString: "7A9160C8-458D-42ED-BC9D-D44068DC6793")!, description: "Test image")
     static var exampleWithoutPhoto = TaggedPhoto.example
+    static var exampleWithPhoto = TaggedPhoto(id: UUID(uuidString: "7A9160C8-458D-42ED-BC9D-D44068DC6793")!, description: "Test image")
+    static var exampleWithPhotoAndLoc = TaggedPhoto(id: UUID(uuidString: "7A9160C8-458D-42ED-BC9D-D44068DC6793")!, description: "Test image", latitude: 51.9977, longitude: 0.7407)
     
     static var previews: some View {
-        TaggedPhotoDetailView(taggedPhoto: exampleWithPhoto, viewModel: ContentView.ViewModel())
         TaggedPhotoDetailView(taggedPhoto: exampleWithoutPhoto, viewModel: ContentView.ViewModel())
+        TaggedPhotoDetailView(taggedPhoto: exampleWithPhoto, viewModel: ContentView.ViewModel())
+        TaggedPhotoDetailView(taggedPhoto: exampleWithPhotoAndLoc, viewModel: ContentView.ViewModel())
     }
 }
