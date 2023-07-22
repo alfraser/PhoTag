52a1957651221332098defb22b8fa5a3f906bc88
alfraser <alisdairfraser@gmail.com>
2023-07-22 15:51:21 +0100

    Added optionality of the location and amended the view to reflect this

--- PhoTag/View/PhotoDetailView.swift
+++ PhoTag/View/PhotoDetailView.swift
@@ -5,12 +5,14 @@
 //  Created by Al Fraser on 18/07/2023.
 //
 
+import MapKit
 import SwiftUI
 
 struct PhotoDetailView: View {
     @State var photo: UIImage
     @State var description: String
     @State var showMap = false
+    @State var location: CLLocationCoordinate2D?
     
     var body: some View {
         ZStack {
@@ -22,35 +24,37 @@ struct PhotoDetailView: View {
                 Text(description)
                     .asLabel()
             }
-            VStack {
-                Spacer()
-                HStack {
-                    if showMap {
-                        Button {
-                            showMap.toggle()
-                        } label: {
-                            Image(systemName: "arrow.down.left.circle")
-                                .padding(8)
-                                .background(.blue)
-                                .foregroundColor(.white)
-                                .font(.title)
-                                .clipShape(Circle())
-                                .padding(.leading)
-                        }
-                    } else {
-                        Button {
-                            showMap.toggle()
-                        } label: {
-                            Image(systemName: "mappin.and.ellipse")
-                                .padding(8)
-                                .background(.blue)
-                                .foregroundColor(.white)
-                                .font(.title)
-                                .clipShape(Circle())
-                                .padding(.leading)
-                        }
-                    }
+            if let loc = location {
+                VStack {
                     Spacer()
+                    HStack {
+                        if showMap {
+                            Button {
+                                showMap.toggle()
+                            } label: {
+                                Image(systemName: "arrow.down.left.circle")
+                                    .padding(8)
+                                    .background(.blue)
+                                    .foregroundColor(.white)
+                                    .font(.title)
+                                    .clipShape(Circle())
+                                    .padding(.leading)
+                            }
+                        } else {
+                            Button {
+                                showMap.toggle()
+                            } label: {
+                                Image(systemName: "mappin.and.ellipse")
+                                    .padding(8)
+                                    .background(.blue)
+                                    .foregroundColor(.white)
+                                    .font(.title)
+                                    .clipShape(Circle())
+                                    .padding(.leading)
+                            }
+                        }
+                        Spacer()
+                    }
                 }
             }
         }
@@ -60,6 +64,10 @@ struct PhotoDetailView: View {
 struct PhotoDetailView_Previews: PreviewProvider {
     static var previews: some View {
         let examplePhoto = UIImage(named: "test")!
-        PhotoDetailView(photo: examplePhoto, description: "A book we liked to read")
+        let loc = CLLocationCoordinate2D(latitude: 51.9977, longitude: 0.7407)
+        return Group {
+            PhotoDetailView(photo: examplePhoto, description: "A book we liked to read")
+            PhotoDetailView(photo: examplePhoto, description: "A book we liked to read", location: loc)
+        }
     }
 }
